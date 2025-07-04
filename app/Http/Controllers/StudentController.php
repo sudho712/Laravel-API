19b6b5f8a3ad3049e98a142d1809dc7e3bf6abf0
Sudhir Kumar <sudho712@gmail.com>
2025-07-04 09:58:39 +0530

    test

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\table_students;
+
+class StudentController extends Controller
+{
+    function list(){
+        return "function called";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get("/test",function(){
     return ["name"=>"raj",'age','25'];
 });
+
+Route::get('students',[StudentController::class,'list']);
